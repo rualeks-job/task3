ee572f6811d9a9387b08f7314b3cfc73b94d0284
rualeks-job <rualeks-job@mail.ru>
2025-04-01 22:53:24 +0300

    task 3. исправление замечаний

--- src/main/java/ru/aleksey/NauJava/configurations/Config.java
+++ src/main/java/ru/aleksey/NauJava/configurations/Config.java
@@ -4,10 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import ru.aleksey.NauJava.NauJavaApplication;
 
 import java.util.Scanner;
 
