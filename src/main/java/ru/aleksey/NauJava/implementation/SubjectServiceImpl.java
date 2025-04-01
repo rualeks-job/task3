ce1533666205d8dfafa71c6055475188f1466dfb
rualeks-job <rualeks-job@mail.ru>
2025-04-01 22:43:20 +0300

    task 4. Практическа работа 4

--- src/main/java/ru/aleksey/NauJava/implementation/SubjectServiceImpl.java
+++ src/main/java/ru/aleksey/NauJava/implementation/SubjectServiceImpl.java
@@ -4,52 +4,40 @@ import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.aleksey.NauJava.objects.Grade;
 import ru.aleksey.NauJava.objects.Subject;
+import ru.aleksey.NauJava.repositories.GradeRepository;
 import ru.aleksey.NauJava.repositories.SubjectRepository;
 import ru.aleksey.NauJava.services.SubjectService;
+
+import java.util.List;
 
 @Service
 public class SubjectServiceImpl implements SubjectService {
     private final SubjectRepository subjectRepository;
-
+    private final GradeRepository gradeRepository;
     private final String appName;
     public final String appVersion;
 
     @Autowired
-    public SubjectServiceImpl(SubjectRepository subjectRepository,
-                              @Value("${app.name}") String appName,
-                              @Value("${app.version}") String appVersion) {
+    public SubjectServiceImpl(
+            SubjectRepository subjectRepository, GradeRepository gradeRepository,
+            @Value("${app.name}") String appName,
+            @Value("${app.version}") String appVersion) {
         this.subjectRepository = subjectRepository;
+        this.gradeRepository = gradeRepository;
         this.appVersion = appVersion;
         this.appName = appName;
     }
 
     @Override
-    public void createSubject(Long id, String title, String teacher, Double duration) {
-        Subject newSubject = new Subject();
-        newSubject.setId(id);
-        newSubject.setTeacher(teacher);
-        newSubject.setTitle(title);
-        newSubject.setDuration(duration);
-        subjectRepository.create(newSubject);
-    }
-
-    @Override
-    public Subject findSubjectById(Long id) {
-        return subjectRepository.read(id);
-    }
-
-    @Override
-    public void deleteSubjectById(Long id) {
-        subjectRepository.delete(id);
-    }
-
-
-    @Override
-    public void updateTeacherById(Long id, String teacher) {
-        Subject oldSubject = subjectRepository.read(id);
-        oldSubject.setTeacher(teacher);
-        subjectRepository.update(oldSubject);
+    @Transactional
+    public void deleteSubjectByTitle(String title) {
+        Subject subject = subjectRepository.findByTitle(title);
+        List<Grade> gradeList = gradeRepository.findAllGradeBySubjectTitle(subject.getTitle());
+        gradeRepository.deleteAll(gradeList);
+        subjectRepository.delete(subject);
     }
 
     @PostConstruct
--- src/main/java/ru/aleksey/NauJava/services/SubjectService.java
+++ src/main/java/ru/aleksey/NauJava/services/SubjectService.java
@@ -1,13 +1,6 @@
 package ru.aleksey.NauJava.services;
 
-import ru.aleksey.NauJava.objects.Subject;
 
 public interface SubjectService {
-    void createSubject(Long id, String title, String teacher, Double duration);
-
-    Subject findSubjectById(Long id);
-
-    void deleteSubjectById(Long id);
-
-    void updateTeacherById(Long id, String teacher);
+    void deleteSubjectByTitle(String title);
 }
